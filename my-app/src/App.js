e225258b0488f1f9b928871b5138abb1934eff37
Alexandra <donchenkoalexandra@gmail.com>
2021-01-28 10:11:59 +0100

    created files for actions and counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,10 @@
 
+import { useSelector } from 'react-redux';
 import './App.css';
 
+
 function App() {
+  const counter = useSelector((state)=> state);
   return (
     <div className="App">
     HELLO
